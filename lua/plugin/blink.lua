8d9e69aa14d8a3c3a9d2211e7f233631e7dc3b23
DarkLionRRR <lllevaa28@gmail.com>
2026-08-05 00:44:25 +0300

    Add autocomplete

---
+++ lua/plugin/blink.lua
@@ -0,0 +1,13 @@
+return {
+    {
+        "Saghen/blink.cmp",
+        version = "v1",
+        opts = {
+            keymap = { preset = "super-tab" },
+            completion = { documentation = { auto_show = true } },
+            appearance = {
+                nerd_font_variant = "normal",
+            },
+        },
+    },
+}
